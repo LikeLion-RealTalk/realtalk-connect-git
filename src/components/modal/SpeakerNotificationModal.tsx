40352680bcd9fcedbccb586d09a49215a47be5e9
june <0721hjp@naver.com>
2025-08-26 01:53:36 +0900

    fix: 모든 모달의 모바일 반응형 디자인 수정

--- src/components/modal/SpeakerNotificationModal.tsx
+++ src/components/modal/SpeakerNotificationModal.tsx
@@ -11,7 +11,7 @@ export function SpeakerNotificationModal({ isOpen, onClose }: SpeakerNotificatio
   return (
     <Dialog open={isOpen} onOpenChange={(open) => !open && onClose()}>
       <DialogContent 
-        className="max-h-[85vh] sm:max-h-[90vh] flex flex-col rounded-2xl border-2 shadow-xl text-center sm:max-w-lg"
+        className="w-[90vw] sm:w-auto sm:max-w-lg max-h-[85vh] sm:max-h-[90vh] flex flex-col rounded-2xl border-2 shadow-xl text-center"
         onEscapeKeyDown={onClose}
         onPointerDownOutside={onClose}
       >
