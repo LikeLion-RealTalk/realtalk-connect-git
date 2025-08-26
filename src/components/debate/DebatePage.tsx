fd758a637297ac3db5b180e6c1cb273cb77cf8f2
june <0721hjp@naver.com>
2025-08-26 16:55:38 +0900

    chore: 원격 비디오 음성 활성화 - muted 속성 제거

--- src/components/debate/DebatePage.tsx
+++ src/components/debate/DebatePage.tsx
@@ -1508,7 +1508,7 @@ export function DebatePage({ onNavigate, onGoBack, debateRoomInfo }: DebatePageP
                           id={`video-${remoteUser.userId}`}
                           autoPlay
                           playsInline
-                          muted={true}
+                          muted={false}
                           className="w-full h-full object-cover"
                         />
                         <div 
